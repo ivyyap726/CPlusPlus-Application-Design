516b1a13d81bbb330f8c9e5c73e45f3d7f81244e
ivyyap726 <aiyinyap1998@gmail.com>
2026-08-29 10:45:00 -0700

    Update hello_world.cpp

--- Module01-Setup/hello_world.cpp
+++ Module01-Setup/hello_world.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-    cout << "My C++ application is ready!" << endl;
+    cout << "Welcome to My Personal Budget Tracker!" << endl;
+    cout << "This app will track my income and expenses by date, category, and amount." << endl;
+
+    // Plan: later I will read transaction data from a CSV file
+    // (columns: Date, Transaction Description, Category, Amount, Type)
+    // Sample dataset: Kaggle "Personal Finance Data" by ramyapintchy
+
     system("pause>0");
+    return 0;
 }
